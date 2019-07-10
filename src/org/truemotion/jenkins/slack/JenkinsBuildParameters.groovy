72b03ffd8077f2a1a680ee48168d5a2258da8f32
Terrence Beach <tjbeachjr@gmail.com>
2019-07-10 13:18:08 -0700

    Potential fix for retrieving build parameters

--- src/org/truemotion/jenkins/slack/JenkinsBuildParameters.groovy
+++ src/org/truemotion/jenkins/slack/JenkinsBuildParameters.groovy
@@ -1,15 +1,16 @@
 package org.truemotion.jenkins.slack
 
-import hudson.model.*
+import hudson.model.ParameterValue
+import hudson.model.ParametersAction
 
 @NonCPS
 String getBuildParameters() {
   def build_params = ""
-  def parameters = currentBuild?.actions.find{ it instanceof ParametersAction }?.parameters
+  def parameters = currentBuild.rawBuild.getAction(ParametersAction.class)
   if (parameters != null) {
     build_params = "Build Parameters:\n"
-    parameters.each {
-      build_params = build_params + ("\t" + it.name + ": " + it.dump() + "\n")
+    for(ParameterValue p in parameters) {
+      build_params = build_params + ("\t" + p.name + ": " + p.value + "\n")
     }
   }
   return build_params
